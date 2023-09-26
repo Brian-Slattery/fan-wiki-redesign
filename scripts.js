000dade9d3236533b785cc14e11b0d15371a06e2
Brian-Slattery <brianjslat@gmail.com>
2023-09-26 17:26:06 -0400

    basic javascript for page done

--- scripts.js
+++ scripts.js
@@ -1,25 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     //top bar variables
-    let searchbar = document.getElementById("search")
-    let searchbar2 = document.getElementById("search2")
+    let searchbar = document.getElementById("search");
+    let searchbar2 = document.getElementById("search2");
 
     //info panel variables
-    let infoButtonOne = document.getElementById("infoButtonOne")
-    let infoButtonTwo = document.getElementById("infoButtonTwo")
-    let infoButtonThree = document.getElementById("infoButtonThree")
+    let infoButtonOne = document.getElementById("infoButtonOne");
+    let infoButtonTwo = document.getElementById("infoButtonTwo");
+    let infoButtonThree = document.getElementById("infoButtonThree");
 
-    let infoCatagories = document.getElementById("catagories")
-    let infoAnswers = document.getElementById("answers")
+    let infoCatagories = document.getElementById("catagories");
 
-    let backgroundCats = document.getElementById("backgroundCats")
-    let apperanceCats = document.getElementById("apperanceCats")
+    let backgroundCats = document.getElementById("backgroundCats");
+    let apperanceCats = document.getElementById("apperanceCats");
 
-    let apperanceAnswers = document.getElementById("apperanceAnswers")
-    let backgroundAnswers = document.getElementById("backgroundAnswers")
-    let affiliationAnswers = document.getElementById("affiliationAnswers")
+    let apperanceAnswers = document.getElementById("apperanceAnswers");
+    let backgroundAnswers = document.getElementById("backgroundAnswers");
+    let affiliationAnswers = document.getElementById("affiliationAnswers");
 
-    let infoPhotos = document.getElementById("infoPic")
+    let infoPhotos = document.getElementById("infoPic");
     let currentImageIndex = 0;
 
     let infoDisplayed = 2;
@@ -30,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //bio panel variables
 
-    let bioContainer = document.getElementById("bioContainer")
+    let bioContainer = document.getElementById("bioContainer");
     let bioTitle = document.getElementById("bioTitle");
 
     let personalityButton = document.getElementById("bioButtonOne");
@@ -39,22 +38,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const personalityButtonWidth = window.getComputedStyle(personalityButton).width;
 
     let bioButton = document.getElementById("bioButtonTwo");
-    let bioDropdown = document.getElementById("bioOptions")
+    let bioDropdown = document.getElementById("bioOptions");
 
     const BioButtonWidth = window.getComputedStyle(bioButton).width;
 
-    let textArea = document.getElementById("bioText")
+    let textArea = document.getElementById("bioText");
 
-    let bioNextPage = document.getElementById("bioNextPage")
-    let bioLastPage = document.getElementById("bioPreviousPage")
+    let bioNextPage = document.getElementById("bioNextPage");
+    let bioLastPage = document.getElementById("bioPreviousPage");
 
-    let bioPageNumber = document.getElementById("bioPageNumber")
+    let bioPageNumber = document.getElementById("bioPageNumber");
 
-    let originsButton = document.getElementById("originsButton")
-    let legacyButton = document.getElementById("legacyButton")
+    let originsButton = document.getElementById("originsButton");
+    let legacyButton = document.getElementById("legacyButton");
+    let btsButton = document.getElementById("bioButtonThree")
+
     let originsText = "Mitth'raw'nuruodo was born on the planet Csilla during the final decades of the Galactic Republic. His species, the Chiss, controlled an area of space in the part of the galaxy known to those in the Republic as the Unknown Regions. He and his brother, Mitth'ras'safis, were born to a family of commoners, but, after joining the Chiss Expansionary Fleet, Mitth'raw'nuruodo was taken as a merit adoptive in the Eighth Ruling Family of Chiss society. Mitth'ras'safis also joined the Eight Ruling Family, eventually becoming a Syndic within the family. Though the practice of military officers becoming merit adoptives in the Ruling Families, and taking on the colors of their adoptive family, was common, they usually ceased belonging to the family upon leaving the military. Mitth'raw'nuruodo, however, soon reached the rank of Force Commander—the youngest Chiss ever to achieve the rank—and impressed the Eighth Ruling Family enough during his early career to be considered a trial-born, with the possibility of becoming a permanent member of the family. His work with the Expansionary Fleet gave him the opportunity to learn to speak several languages used throughout the Unknown Regions, including the trade languages Sy Bisti and Minnisiat, in addition to his native Cheunh."
     let legacyText = "After his death, Thrawn would be remembered as the genius tactician, consummate strategist, and Imperial leader who nearly brought down the New Republic. Thrawn was viewed as a hero by some members of the Empire of the Hand, including one of Baron Fel's sons, the Chiss-raised Jagged Fel. However, most of his own people still intensely disliked him. During a battle between Chiss forces and a group of pirates at the Syndic Mitth'raw'nuruodo training academy on Rhigar, Lieutenant Jagged Fel used Thrawn's tactics as an inspiration as he and the surviving Chiss cadets defeated the pirates. Fel would go on to become the Galactic Emperor of a revived empire."
+    let btsText = "Grand Admiral Thrawn first appeared in Timothy Zahn's Heir to the Empire, the first book of The Thrawn Trilogy named for the Grand Admiral. The trilogy marked the dawn of a new era in the Star Wars Expanded Universe, and Thrawn's character is considered a contributing factor.[70] Author Timothy Zahn has stated that Thrawn's character is a composite of various historical and fictional personalities, including Erwin Rommel, Robert E. Lee, Hannibal Barca, Alexander the Great and Sherlock Holmes, to name a few.[71] Another loose inspiration for Thrawn was the character of General Maximilian Veers from the 1980 film Star Wars: Episode V The Empire Strikes Back, who had the distinction of being one of the few highly competent Imperial officers from the original trilogy. In a 2007 interview, Timothy Zahn explained that he could bring another clone of Thrawn to life if the Grand Admiral's ability is an absolute necessity to solve future galactic crisis. However, to escape the traditional archetypes of clones, Zahn stated that that clone would be aware that he was not Thrawn, and would be aware of immense pressure to live up to the original. Thrawn's possible return was hinted at in Zahn's novel Survivor's Quest, where great changes in the behavior of Chaf'orm'bintrano lead Mara Jade Skywalker to suspect that another clone of Thrawn existed and may be behind the events of the novel."
+    //appearance Panel Variables
 
+    let appearancesButton = document.getElementById("appearancesButton");
+    let MentionedButton = document.getElementById("MentionedButton");
+
+    let appearancesContainer = document.getElementById("appearances");
+    let mentionsContainer = document.getElementById("mentions");
 
     //---------------------------------------------------------
 
@@ -141,13 +149,7 @@ document.addEventListener('DOMContentLoaded', () => {
         return pages;
     }
 
-    //thing to edit to 
-
-
     let devidedPages = divideText(originsText);
-
-
-
 
     let currentPage = 0;
     function displaySinglePage(pageNumber) {
@@ -240,6 +242,7 @@ document.addEventListener('DOMContentLoaded', () => {
         displaySinglePage(currentPage); 
         bioButton.style.backgroundColor = "rgb(57, 57, 57)"
         personalityButton.style.backgroundColor = "rgb(83, 83, 83)"
+        btsButton.style.backgroundColor = "rgb(83, 83, 83)"
         bioTitle.textContent = "Origins"
         bioContainer.style.background = "linear-gradient(rgba(164, 164, 164, 0.5), rgba(164, 164, 164, 0.5)), url(photos/testBackground.jpg)"
         bioContainer.style.backgroundSize = "cover"
@@ -251,10 +254,42 @@ document.addEventListener('DOMContentLoaded', () => {
         currentPage = 0;
         displaySinglePage(currentPage);
         bioButton.style.backgroundColor = "rgb(83, 83, 83)"
+        btsButton.style.backgroundColor = "rgb(83, 83, 83)"
         personalityButton.style.backgroundColor = "rgb(57, 57, 57)"
         bioTitle.textContent = "Legacy"
         bioContainer.style.background = "linear-gradient(rgba(164, 164, 164, 0.5), rgba(164, 164, 164, 0.5)), url(photos/Media1.jpg)"
         bioContainer.style.backgroundSize = "cover"
     });
+
+    btsButton.addEventListener('click', () => {
+        devidedPages = divideText(btsText)
+        currentPage = 0;
+        displaySinglePage(currentPage);
+        btsButton.style.backgroundColor = "rgb(57, 57, 57)"
+        bioButton.style.backgroundColor = "rgb(83, 83, 83)"
+        personalityButton.style.backgroundColor = "rgb(83, 83, 83)"
+        bioTitle.textContent = "Behind the Scenes"
+        bioTitle.style.whiteSpace = "nowrap"
+        if (window.innerWidth <= 1500) {
+            bioTitle.style.whiteSpace = "normal"
+            bioTitle.style.textAlign = "center"
+        }
+
+    })     
+
+    //appearance Panel
+    appearancesButton.addEventListener('click', () => {
+        appearancesButton.style.backgroundColor = "rgb(57, 57, 57)"
+        appearancesContainer.style.display = "flex"
+        mentionsContainer.style.display = "none"
+        MentionedButton.style.backgroundColor = "rgb(83, 83, 83)"
+    })
+
+    MentionedButton.addEventListener('click', () => {
+        appearancesButton.style.backgroundColor = "rgb(83, 83, 83)"
+        appearancesContainer.style.display = "none"
+        mentionsContainer.style.display = "flex"
+        MentionedButton.style.backgroundColor = "rgb(57, 57, 57)"
+    })
 
 });  
